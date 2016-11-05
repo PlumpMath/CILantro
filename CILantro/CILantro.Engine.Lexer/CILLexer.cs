3763048410d58458f74ded34a042ce352a02d312
kamil <kamil@KK-MSI>
2016-11-05 06:13:41 +0100

    Lexer - removing unneeded white spaces.

--- CILantro/CILantro.Engine.Lexer/CILLexer.cs
+++ CILantro/CILantro.Engine.Lexer/CILLexer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CILantro.Engine.Lexer
 {
@@ -6,6 +7,8 @@ namespace CILantro.Engine.Lexer
     {
         public List<CILToken> Tokenize(string sourceCode)
         {
+            var codeWithoutWhiteSpaces = Regex.Replace(sourceCode, @"\s+", " ");
+
             return new List<CILToken>();
         }
     }
