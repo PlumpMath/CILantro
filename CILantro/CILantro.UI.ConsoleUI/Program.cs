aa165fe6c38162c4d3fe3a605a7141c7a3db51ed
Kamil Kwaśny <Kamil Kwany>
2016-11-05 04:29:42 +0100

    Initial solution.

---
+++ CILantro/CILantro.UI.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CILantro.UI.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
