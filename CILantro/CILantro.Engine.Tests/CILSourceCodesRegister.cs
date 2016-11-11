3c25b4cd6e472b16120810f0ae39cdcc504ddd1b
kamil <kamil@KK-MSI>
2016-11-11 03:09:54 +0100

    Test method for testing engine on sample source codes has been added.

---
+++ CILantro/CILantro.Engine.Tests/CILSourceCodesRegister.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace CILantro.Engine.Tests
+{
+    internal static class CILSourceCodesRegister
+    {
+        private static string SourceCodesDirectoryName = "CILSourceCodes";
+
+        private static List<string> FileNames = new List<string>
+        {
+            "001_empty.il",
+            //"002_read_key.il"
+        };
+
+        public static List<string> SourceCodes = FileNames.Select(fn => GetSourceCode(fn)).ToList();
+
+        private static string GetSourceCode(string fileName)
+        {
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uriBuilder = new UriBuilder(codeBase);
+            var assemblyPath = Uri.UnescapeDataString(uriBuilder.Path);
+            var assemblyDirPath = Path.GetDirectoryName(assemblyPath);
+            var filePath = Path.Combine(assemblyDirPath, SourceCodesDirectoryName, fileName);
+            var sourceCode = File.ReadAllText(filePath);
+
+            return sourceCode;
+        }
+    }
+}
---
+++ CILantro/CILantro.Engine.Tests/CILantroEngineTests.cs
@@ -0,0 +1,26 @@
+﻿using CILantro.Engine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+using Xunit.Extensions;
+
+namespace CILantro.Engine.Tests
+{
+    public class CILantroEngineTests
+    {
+        private CILantroEngine _engine = new CILantroEngine();
+
+        public static List<object[]> SourceCodes = CILSourceCodesRegister.SourceCodes.Select(sc => new object[] { sc }).ToList();
+
+        [Theory, MemberData(nameof(SourceCodes))]
+        public void ShouldCorrectlyInterpretSourceCodes2(string sourceCode)
+        {
+            var consoleReader = new StreamReader(Console.OpenStandardInput());
+            var consoleWriter = new StreamWriter(Console.OpenStandardOutput());
+
+            _engine.Process(sourceCode, consoleReader, consoleWriter);
+        }
+    }
+}
