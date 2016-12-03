9dce38aae5e564e9b24e6e1026d5f611da2676a0
kamil <kamil@KK-MSI>
2016-12-03 13:05:25 +0100

    Program 002 parsing.

--- CILantro/CILantro.Engine.Parser/CILGrammar.cs
+++ CILantro/CILantro.Engine.Parser/CILGrammar.cs
@@ -15,19 +15,26 @@ namespace CILantro.Engine.Parser
 
             // keywords
 
+            var callKeyword = ToTerm("call");
             var cilKeyword = ToTerm("cil");
+            var externKeyword = ToTerm("extern");
             var managedKeyword = ToTerm("managed");
-            var returnKeyword = ToTerm("ret");
+            var popKeyword = ToTerm("pop");
+            var retKeyword = ToTerm("ret");
             var staticKeyword = ToTerm("static");
+            var valuetypeKeyword = ToTerm("valuetype");
             var voidKeyword = ToTerm("void");
 
             // punctuation
 
             var dot = ToTerm(".");
+            var colon = ToTerm(":");
             var leftBrace = ToTerm("{");
             var rightBrace = ToTerm("}");
             var leftParenthesis = ToTerm("(");
             var rightParenthesis = ToTerm(")");
+            var leftBracket = ToTerm("[");
+            var rightBracket = ToTerm("]");
 
             // identifiers
 
@@ -36,13 +43,36 @@ namespace CILantro.Engine.Parser
             var namespaceIdentifier = new NonTerminal("namespaceIdentifier");
             namespaceIdentifier.Rule = identifier | (identifier + dot + namespaceIdentifier);
 
+            var typeIdentifier = new NonTerminal("typeIdentifier");
+            typeIdentifier.Rule = identifier | (identifier + dot + typeIdentifier);
+
+            var methodIdentifier = new NonTerminal("methodIdentifier");
+            methodIdentifier.Rule = typeIdentifier + colon + colon + identifier + leftParenthesis + rightParenthesis;
+
+            // instructions
+
+            var callInstruction = new NonTerminal("callInstruction");
+            callInstruction.Rule = callKeyword + valuetypeKeyword + leftBracket + namespaceIdentifier + rightBracket + typeIdentifier + leftBracket + namespaceIdentifier + rightBracket + methodIdentifier;
+
+            var popInstruction = new NonTerminal("popInstruction");
+            popInstruction.Rule = popKeyword;
+
+            var retInstruction = new NonTerminal("retInstruction");
+            retInstruction.Rule = retKeyword;
+
+            var instruction = new NonTerminal("instruction");
+            instruction.Rule = callInstruction | popInstruction | retInstruction;
+
+            var instructionsSequence = new NonTerminal("instructionsSequence");
+            instructionsSequence.Rule = instruction | (instruction + instructionsSequence);
+
             // methods
 
             var methodParameters = new NonTerminal("methodParameters");
             methodParameters.Rule = leftParenthesis + rightParenthesis;
 
             var methodDeclBlock = new NonTerminal("methodDeclarationBlock");
-            methodDeclBlock.Rule = leftBrace + entryPointKeyword + returnKeyword + rightBrace;
+            methodDeclBlock.Rule = leftBrace + entryPointKeyword + instructionsSequence + rightBrace;
 
             var methodDeclaration = new NonTerminal("methodDeclaration");
             methodDeclaration.Rule = methodDeclKeyword + (staticKeyword | Empty) + voidKeyword + identifier + methodParameters + cilKeyword + managedKeyword + methodDeclBlock;
@@ -61,12 +91,15 @@ namespace CILantro.Engine.Parser
             assemblyDeclBlock.Rule = leftBrace + rightBrace;
 
             var assemblyDeclaration = new NonTerminal("assemblyDeclaration");
-            assemblyDeclaration.Rule = assemblyDeclKeyword + identifier + assemblyDeclBlock;
+            assemblyDeclaration.Rule = assemblyDeclKeyword + (externKeyword | Empty) + identifier + assemblyDeclBlock;
+
+            var assemblyDeclarations = new NonTerminal("assemblyDeclaration");
+            assemblyDeclarations.Rule = assemblyDeclaration | (assemblyDeclaration + assemblyDeclarations);
 
             // program
 
             var program = new NonTerminal("program");
-            program.Rule = assemblyDeclaration + classDeclaration;
+            program.Rule = assemblyDeclarations + classDeclaration;
 
             // root
 
