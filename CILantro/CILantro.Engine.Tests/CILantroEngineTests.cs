d662289255c8607b8abf35e3d73f69b7c3e4d9bc
kamil <kamil@KK-MSI>
2016-12-10 16:23:10 +0100

    Test data moved outside the solution.

--- CILantro/CILantro.Engine.Tests/CILantroEngineTests.cs
+++ CILantro/CILantro.Engine.Tests/CILantroEngineTests.cs
@@ -15,51 +15,25 @@ namespace CILantro.Engine.Tests
 
         private readonly string OutputDataFileExtension = ".out";
 
-        private readonly string SourceCodesDirectoryName = "CILSourceCodes";
+        private readonly string SourceCodesDirectoryPath = @"D:\UWr\mgr\semestr IV\CILantro\CILSourceCodes";
 
-        private readonly string InputDataDirectoryName = "CILInputData";
+        private readonly string InputDataDirectoryName = "in";
 
-        private readonly string OutputDataDirectoryName = "CILOutputData";
-
-        private string WorkingDirPath
-        {
-            get
-            {
-                var codeBase = Assembly.GetExecutingAssembly().CodeBase;
-                var uriBuilder = new UriBuilder(codeBase);
-                var assemblyPath = Uri.UnescapeDataString(uriBuilder.Path);
-                var assemblyDirPath = Path.GetDirectoryName(assemblyPath);
-                return assemblyDirPath;
-            }
-        }
-
-        private string SourceCodesDirectoryPath
-        {
-            get { return Path.Combine(WorkingDirPath, SourceCodesDirectoryName); }
-        }
-
-        private string InputDataDirectoryPath
-        {
-            get { return Path.Combine(WorkingDirPath, InputDataDirectoryName); }
-        }
-
-        private string OutputDataDirectoryPath
-        {
-            get { return Path.Combine(WorkingDirPath, OutputDataDirectoryName); }
-        }
+        private readonly string OutputDataDirectoryName = "out";
 
         [Theory]
-        [InlineData("0001_empty", "0001", "0001")]
+        [InlineData("empty", "empty", "empty")]
         public void ShouldCorrectlyInterpretSourceCodes(string programName, string inputDataName, string outputDataName)
         {
             Console.WriteLine(programName);
 
             var sourceCodeFileName = programName + SourceCodeFileExtension;
-            var sourceCodePath = Path.Combine(SourceCodesDirectoryPath, sourceCodeFileName);
+            var sourceCodePath = Path.Combine(SourceCodesDirectoryPath, programName, sourceCodeFileName);
             var sourceCode = File.ReadAllText(sourceCodePath);
 
+            var inputDataDirectoryPath = Path.Combine(SourceCodesDirectoryPath, programName, InputDataDirectoryName);
             var inputDataFileName = inputDataName + InputDataFileExtension;
-            var inputDataPath = Path.Combine(InputDataDirectoryPath, programName, inputDataFileName);
+            var inputDataPath = Path.Combine(inputDataDirectoryPath, inputDataFileName);
 
             using (var inputDataStream = new StreamReader(inputDataPath))
             {
