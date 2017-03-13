41f9d9ebfc698db578eb6e92601a76b4bf65555a
kamil <kamil@KK-MSI>
2017-03-13 02:52:31 +0100

    Interpreting program 17 with new grammar.

--- CILantro/CILantro.Engine.AST/ASTNodes/Instructions/AddInstruction.cs
+++ CILantro/CILantro.Engine.AST/ASTNodes/Instructions/AddInstruction.cs
@@ -1,12 +1,16 @@
-﻿using System;
-
-namespace CILantro.Engine.AST.ASTNodes.Instructions
+﻿namespace CILantro.Engine.AST.ASTNodes.Instructions
 {
     public class AddInstruction : InstructionNone
     {
         public override CILInstruction Execute(CILProgram program, CILProgramState state)
         {
-            throw new NotImplementedException();
+            var argument1 = (int)state.Stack.Pop();
+            var argument2 = (int)state.Stack.Pop();
+
+            var result = argument1 + argument2;
+            state.Stack.Push(result);
+
+            return Method.GetNextInstruction(this);
         }
     }
 }
