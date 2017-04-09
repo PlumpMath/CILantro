cc043a818f5c784f0e85f527d3d874963a930427
kamil <kamil@KK-MSI>
2017-04-09 17:26:11 +0200

    Interpreting program 32.

--- CILantro/CILantro.Engine.AST/ASTNodes/Instructions/LeaveInstruction.cs
+++ CILantro/CILantro.Engine.AST/ASTNodes/Instructions/LeaveInstruction.cs
@@ -1,20 +1,12 @@
-﻿using System;
-
-namespace CILantro.Engine.AST.ASTNodes.Instructions
+﻿namespace CILantro.Engine.AST.ASTNodes.Instructions
 {
     public class LeaveInstruction : InstructionBranch
     {
-        public override int BytesLength
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public override int BytesLength => 5;
 
         public override CILInstruction Execute(CILProgram program, CILProgramState state)
         {
-            throw new NotImplementedException();
+            return Method.GetInstructionByBranchTarget(this, Target);
         }
     }
 }
